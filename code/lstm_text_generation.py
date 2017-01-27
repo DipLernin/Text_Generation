182a09941f13eb183b22feb66a6c871395b6cb98
krishnakalyan3 <krishnakalyan3@gmail.com>
2017-01-27 11:31:41 +0100

    model checkpoint

--- code/lstm_text_generation.py
+++ code/lstm_text_generation.py
@@ -20,7 +20,7 @@ import numpy as np
 import random
 import sys
 
-path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
+path = '../datasets/Homer_odyssey_Iliad.txt'
 text = open(path).read().lower()
 print('corpus length:', len(text))
 
@@ -74,7 +74,7 @@ for iteration in range(1, 60):
     print('-' * 50)
     print('Iteration', iteration)
     model.fit(X, y, batch_size=128, nb_epoch=1, validation_split=0.2)
-
+    checkpointer = ModelCheckpoint(filepath="../models/model_" + i + ".hdf5", verbose=1, save_best_only=True)
     start_index = random.randint(0, len(text) - maxlen - 1)
 
     for diversity in [0.2, 0.5, 1.0, 1.2]:
